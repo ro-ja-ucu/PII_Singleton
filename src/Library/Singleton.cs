907a161cbaffebd35eaad8cc74edb13e00e86b3f
Rodrigo S. Jauregui <rodrigo.jauregui@correo.ucu.edu.uy>
2021-09-27 10:56:56 -0300

    Singleton generico funcional.

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,21 @@
+namespace Library
+{
+    public class Singleton<T> where T : new()
+    {
+        
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+
+    }
+}
